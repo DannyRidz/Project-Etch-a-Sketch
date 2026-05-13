a42f4159179b5e1c4f7b5e3510b637100837751f
DannyRidz <danny.ridz93@gmail.com>
2026-05-13 14:08:38 +0800

    Set up a "hover" effect on the grid squares.
    I added the following code to script.js to create a hover effect on the grid squares. When the user hovers over a square, its background color changes to blue.

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'blue';
+    })
     container.appendChild(div);
 }
